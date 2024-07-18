ebbc1b147bfee938e9b73ce78b69a9fdd592cd9f
Qiu JiaNan <1822533929@qq.com>
2024-07-18 16:42:09 +0800

    "入库代码"

--- wms-server/src/main/java/com/ikun/wms/controller/instoreController.java
+++ wms-server/src/main/java/com/ikun/wms/controller/instoreController.java
@@ -1,8 +1,14 @@
 package com.ikun.wms.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/instore")
 public class instoreController {
+    @RequestMapping("/instore")
+    public String instore(){
+        return "instore";
+    }
 
 }
