81b7b7b428c2ee7b5b75bc340894b6cf1cd6b45c
yiwang <yiwang0261@qq.com>
2024-07-22 10:08:46 +0800

    查询条件封装

---
+++ wms-server/src/main/java/com/ikun/wms/pojo/query/BaseQuery.java
@@ -0,0 +1,14 @@
+package com.ikun.wms.pojo.query;
+
+import lombok.Data;
+
+/**
+ * @Author: yiwang
+ * @Date: 2024/7/22
+ */
+
+@Data
+public class BaseQuery {
+    private Integer currentPage;
+    private Integer pageSize;
+}
